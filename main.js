5b260d19d8d0c1c4aca7d53193a47e7ac4f002f4
Demidov <classen.vika@yandex.ru>
2024-09-04 13:35:35 +0500

    lesson2

---
+++ main.js
@@ -0,0 +1,70 @@
+// console.log("hello");
+// console.log(2+2);
+// console.log('hello'+' world');
+
+// alert('hello world')
+
+// 1 вариант, лучше не использавать
+// var a = 10
+// a = 20
+// console.log(a);
+
+
+// let b=1998
+// b=2024
+// console.log(b);
+
+// const c=20;
+// console.log(c);
+
+let num = 1
+console.log(num);
+
+console.log('Добро '+'пожаловать'+' на курс'+' Максим');
+
+// alert('04.09.2024')
+
+//тип данных number
+//infinity
+
+let a = Infinity
+console.log(a);
+
+//NaN
+console.log(NaN);
+console.log('str'* 10);
+
+
+//тип данных bigint
+console.log(45567899010928836545n);
+
+//string
+let str1 = 'hello1'
+let str2 = "hello2"
+let str3 = `hello3 ${str1}`
+console.log(str1);
+console.log(str2);
+console.log(str3);
+
+//bool
+
+console.log(20<100);
+console.log(20>100);
+
+//null
+console.log(null);
+
+//undefined
+
+let m;
+console.log(m);
+
+//typeof x
+console.log(typeof 'cat');
+console.log(typeof null);
+console.log(typeof 10);
+console.log(typeof false);
+console.log(typeof 10n);
+console.log(typeof undefined);
+
+document.getElementById('one').innerHTML = 'Hello, world!'
